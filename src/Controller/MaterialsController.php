5681e1bc4f961b3514cde2a2a7b56f1fd0425bae
BanShiro <matheosturaro@hotmail.com>
2024-02-20 15:20:11 +0100

    Ajout formulaires ajout et edit

--- src/Controller/MaterialsController.php
+++ src/Controller/MaterialsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\MaterialFormType;
 use App\Repository\MaterialRepository;
+use App\Entity\Material;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MaterialsController extends AbstractController
@@ -22,6 +26,46 @@ class MaterialsController extends AbstractController
     {
         return $this->render('materials/detail.html.twig' , [
             'materials' =>$materialRepository->findAll()
+        ]);
+    }
+    #[Route('/materials/create', name: 'materials_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager) : Response 
+    {
+       
+        $material = new Material();
+
+        $form = $this->createForm(MaterialFormType::class, $material);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($material);
+            $entityManager->flush();
+           return $this->redirectToRoute('materials_index');
+        }
+        return $this->render('materials/create.html.twig',[
+            'material_form' => $form
+        ]);
+    }
+    #[Route('/materials/{id}', name: 'materials_edit')]
+    public function edit(Material $material,Request $request, EntityManagerInterface $entityManager)
+    {
+
+        $form = $this->createForm(MaterialFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $material = $form->getData();
+            $entityManager->persist($material);
+            $entityManager->flush();
+
+            return new Response('Création du nouveau matériel avec succes');
+
+        }
+        dd($form);
+        return $this->render('materials/edit.html.twig',[
+            'material_form' => $form
         ]);
     }
 }
