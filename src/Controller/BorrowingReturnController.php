f108d23a62f3bd2765c1bedd5c621972dc824e2f
Magic-80 <developper.magic@gmail.com>
2024-03-27 09:44:33 +0100

    add form in borrowing controller

--- src/Controller/BorrowingReturnController.php
+++ src/Controller/BorrowingReturnController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Borrowing;
+use App\Form\BorrowingFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BorrowingReturnController extends AbstractController
 {
     #[Route('/borrowing', name: 'borrowing_index')]
-    public function index(): Response
+    public function index(Request $request , EntityManagerInterface $entityManager): Response
     {
-        return $this->render('borrowing_return/index.html.twig');
+        $borrowing = new Borrowing();
+
+        $form = $this->createForm(BorrowingFormType::class, $borrowing);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($borrowing);
+            $entityManager->flush();
+            return $this->redirectToRoute('borrowing_index');
+        }
+
+        return $this->render('borrowing/index.html.twig', [
+            'borrowing_form' => $form->createView()
+        ]);
     }
 }
